48b4c852073d0f09543b994a5a7d06f551a79eb6
Hugo Hirsch <github@hugo-hirsch.de>
2020-02-17 00:57:49 +0100

    Add docu

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <h1>Welcome</h1>
 <?php
 
-echo date("Y-m-d H:i:s");
+echo "Time: ".date("Y-m-d H:i:s");
 $server = $_SERVER['SERVER_NAME'];
 echo "<br/>Servername:".$server;
 echo '<br/>Contact: <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;'.$server.'">'.$server.'</a>';
