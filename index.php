49c25acccbfc1f402f08179611ea15068502b516
Hugo Hirsch <github@hugo-hirsch.de>
2020-02-17 00:57:07 +0100

    Fix markup

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 echo date("Y-m-d H:i:s");
 $server = $_SERVER['SERVER_NAME'];
 echo "<br/>Servername:".$server;
-echo '<br /><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;'.$server.'">'.$server.'</a>';
+echo '<br/><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;'.$server.'">'.$server.'</a>';
 ?>
 </body>
 </html>
