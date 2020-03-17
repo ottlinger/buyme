7e3c8d5cb4a2a32b19ab303acc6105aee6d4c36c
Hugo Hirsch <github@hugo-hirsch.de>
2020-03-17 21:30:23 +0100

    Fix landing page

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ $hostnamePrinter = new HostnamePrinter();
     <title><?php echo $hostnamePrinter->hostname(); ?> &mdash; powered by BuyMe</title>
 </head>
 <body>
-<h1>The domain <i><?php echo $hostnamePrinter->hostname(); ?></i> is for sale</h1>
+<h1>The domain <i><?php echo $hostnamePrinter->hostname(); ?></i> is for sale :-)</h1>
 <h1>Die Domain <i><?php echo $hostnamePrinter->hostname(); ?></i> steht zum Verkauf</h1>
 <img src="https://picsum.photos/200/300" alt="Just a picture">
 <h2>Get in touch with us / bitte kontaktieren Sie uns</h2>
-<p>via <?php echo $hostnamePrinter->maillink('E-Mail')?></p>
+<p>via <?php echo $hostnamePrinter->maillink('E-Mail') ?></p>
 <hr/>
 <?php
-echo 'Delivered at ' . $hostnamePrinter->time() . ' by <a href="https://github.com/ottlinger/buyme">BuyMe</a>';
+echo 'Delivered at ' . $hostnamePrinter->time() . ' by <a href="https://github.com/ottlinger/buyme" target="_blank">BuyMe</a>';
 ?>
 </body>
 </html>
